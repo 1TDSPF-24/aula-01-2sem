0c9d3bd90b1dad3f7fbc026a8e6e09bb4636bcbd
leoc7sar <lcrn1102@gmail.com>
2024-08-05 09:01:45 -0300

    FOR OF e IN.

--- js/script.js
+++ js/script.js
@@ -7,7 +7,19 @@
    console.table(FRUTAS);
    console.log(FRUTAS[3]);
 
-   //Lendo array
+   //Lendo array COM Loop FOR tradicional
    for(let x = 0; x < FRUTAS.length ; x++){
     console.log(FRUTAS[x]);
+   }
+
+   //Lendo array com Loop FOR OF
+
+   for (const FRUTAS of FRUTAS) {
+    console.log("COM FOR OF", FRUTAS);
+   }
+
+   //Lendo array com Loop FOR IN
+
+   for (const indice in FRUTAS) {
+    console.log("COM FOR IN", FRUTAS[indice]);
    }
