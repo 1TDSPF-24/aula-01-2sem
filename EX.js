4281702123c0f118fc108422ff03b33fe74e1fd1
paulohenrique63 <paulo.golova@gmail.com>
2024-08-05 08:56:15 -0300

    arrays no JS

--- EX.js
+++ EX.js
@@ -1,1 +1,9 @@
 console.log("bem vindo")
+
+let frutas = ["banana", "uva", "laranja"];
+console.table(frutas);
+console.log(frutas[1]);
+
+for (let x = 0; x < frutas.length; x++) {
+    console.table(frutas[x]);
+}
