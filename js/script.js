500f8b069e894dc5f34442e848770cedffca2c34
DuGiovannini <egiovannini5@gmail.com>
2024-08-05 09:01:45 -0300

    FOR OF e FOR IN

--- js/script.js
+++ js/script.js
@@ -11,3 +11,13 @@ console.log(frutas[3]);
 for (let x = 0; x < frutas.length; x++) {
     console.log(frutas[x]);
 }
+
+// Lendo array com loop FOR OF
+for (const fruta of fruta) {
+    console.log("COM FOR OF", fruta)
+}
+
+// Lendo array com loop FOR IN
+for (const indice in frutas) {
+    console.log(" COM FOR IN", frutas[indice]);
+}
