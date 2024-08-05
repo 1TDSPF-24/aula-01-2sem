cd3e040ff9c582419e38823b00c334906bc71405
DuGiovannini <egiovannini5@gmail.com>
2024-08-05 08:57:11 -0300

    For tradicional

--- js/script.js
+++ js/script.js
@@ -1,2 +1,13 @@
+// Criando um array básico de frutas
 
+let frutas = ["banana", "laranja", "caqui", "morango", "uva"];
 
+// Vizualizando o conteúdo do array!
+console.log(frutas);
+console.table(frutas);
+console.log(frutas[3]);
+
+// Lendo array com loop FOR
+for (let x = 0; x < frutas.length; x++) {
+    console.log(frutas[x]);
+}
