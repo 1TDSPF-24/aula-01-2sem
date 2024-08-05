8815e23699adba948857cd646bfc02bae95ab6c3
tcguus <gugucram15@gmail.com>
2024-08-05 08:56:06 -0300

    FOR tradicional.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
-console.log("Bem-Vindo novamente!");
+
+
+//Criando um array basico de frutas
+
+let frutas = ["banana","laranja","caqui","morango","uva" ]
+
+//Visualizando o conteúdo do array!
+console.log(frutas);
+console.table(frutas);
+console.log(frutas[3]);
+
+//lendo array com lop FOR TRADICIONAL 
+for(let x = 0; x <frutas.length ; x++){
+    console.log(frutas[x]);
+}
