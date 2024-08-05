c1849f932bfb053030cb6804277fcd391199e394
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-08-05 09:48:48 -0300

    PUSH,UNSHIFT,SHIFT,POP,SORT e REVERSE.

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 //Criando um array básico de FRUTAS
 
-let frutas = ["banana","laranja","caqui","morango","uva"];
+let frutas = ["uva","caqui","banana","laranja","morango"];
 
 //Visualizando o conteúdo do array!
 console.log(frutas);
@@ -37,3 +37,53 @@ frutas.forEach((frutaDoArray, indiceDoArray, proprioArray)=>{
 
     console.log("COM FOREACH",frutaDoArray);    
 });
+
+
+//Adicionando um novo item ao ARRAY de FRUTAS com PUSH no final.
+frutas.push("melancia");
+
+//Lendo array com Loop FOR OF
+
+for (const fruta of frutas) {
+    console.log("COM FOR OF",fruta);   
+}
+
+//Adicionando um novo item ao ARRAY de FRUTAS com UNSHIFT no início.
+frutas.unshift("pêra");
+
+//Lendo array com Loop FOR OF
+
+for (const fruta of frutas) {
+    console.log("COM FOR OF",fruta);   
+}
+
+//Removendo um item, do final do array com POP;
+frutas.pop();
+//Lendo array com Loop FOR OF
+for (const fruta of frutas) {
+    console.log("COM FOR OF",fruta);   
+}
+
+//Removendo um item, do início do array com SHIFT;
+frutas.shift();
+//Lendo array com Loop FOR OF
+for (const fruta of frutas) {
+    console.log("COM FOR OF",fruta);   
+}
+
+//Colocando em ordem alfabética com SORT;
+frutas.sort();
+//Lendo array com Loop FOR OF
+for (const fruta of frutas) {
+    console.log("COM FOR OF",fruta);   
+}
+
+//Revertendo a ordem apresentanda com REVERSE;
+frutas.reverse();
+//Lendo array com Loop FOR OF
+console.log("_________");   
+for (const fruta of frutas) {
+    console.log("COM FOR OF",fruta);   
+}
+
+
