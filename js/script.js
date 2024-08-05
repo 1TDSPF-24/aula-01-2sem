89bddd0ab64aa92d74b0aed45582a3759a2ac478
lukashibuya <uraluka@uol.com.br>
2024-08-05 09:01:51 -0300

    For off For in

---
+++ js/script.js
@@ -0,0 +1,25 @@
+//Criando um array basico de Frutas
+
+let frutas = ["banna", "laranja", "caqui", "morango", "uva"];
+
+//visualizando o conteudo do array
+console.log(frutas);
+console.table(frutas);
+console.log(frutas[3]);
+
+//Lendo array com Loop FOR TRADICIONAL
+for (let x = 0; x < frutas.length; x++) {
+    console.log(frutas[x]);
+}
+
+//Lendo array com Loop FOR OF
+
+for (const fruta of frutas) {
+    console.log("COM FOR OF", fruta);
+}
+
+//Lendo array com Loop FOR IN
+
+for (const indice in frutas) {
+    console.log("COM FOR IN", frutas[indice]);
+}
