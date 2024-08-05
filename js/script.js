290e181bd3b5e1cdea7f2ed726914ff81ca369ce
gtheox <gteodoro197@gmail.com>
2024-08-05 08:55:59 -0300

    for tradicional

---
+++ js/script.js
@@ -0,0 +1,15 @@
+//Criando um array básico de FRUTAS
+
+let frutas = ["banana", "laranja", "caqui", "morango", "uva"];
+
+//vizualizando o conteudo do array
+
+console.log(frutas);
+console.table(frutas);
+console.log(frutas[3]);
+
+//Lendo array com Loop FOR
+
+for (let x = 0; x < frutas.length; x++) {
+    console.log(frutas[x]);
+}
