7f24a7178f9aeef7dc5fa671ebffdefe1546d920
Júlio-CRodrigues <juliocesarccr29@gmail.com>
2024-08-05 08:56:31 -0300

    FOR tradicional

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+
+//Criando um array básico de FRUTAS
+
+let frutas = ["banana", "laranja", "caqui", "morango", "uva"];
+
+//Visualizando o conteúdo do array!
+console.log(frutas);
+console.table(frutas);
+console.log(frutas[3]);
+
+//lendo array com loop FOR
+for (let x = 0; x < frutas.length; x++) {
+    console.log(frutas[x]);
+}
