b247c76469f6e5866663b448ff266fe33d4d3d02
leoc7sar <lcrn1102@gmail.com>
2024-08-05 08:56:09 -0300

    For tradicional.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 
 
-   console.log("Bem vindo novamente");
+   //Criando um array básico de FRUTAS
+
+   let FRUTAS = ["banana","laranja","caqui","morango","uva"];
+   console.log(FRUTAS);
+   console.table(FRUTAS);
+   console.log(FRUTAS[3]);
+
+   //Lendo array
+   for(let x = 0; x < FRUTAS.length ; x++){
+    console.log(FRUTAS[x]);
+   }
