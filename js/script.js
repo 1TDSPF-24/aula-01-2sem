586a768704382ecbdb8078c75f4be1f0e180fb62
gtheox <gteodoro197@gmail.com>
2024-08-05 09:01:38 -0300

    FOR OF e IN

--- js/script.js
+++ js/script.js
@@ -13,3 +13,15 @@ console.log(frutas[3]);
 for (let x = 0; x < frutas.length; x++) {
     console.log(frutas[x]);
 }
+
+//Lendo array com Loop FOR OF
+
+for (const fruta of frutas) {
+    console.log("COM FOR OF", fruta);
+}
+
+//Lendo array com Loop FOR IN
+
+for (const indice in frutas) {
+    console.log("COM FOR IN", frutas[indice]);
+}
