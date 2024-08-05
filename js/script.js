d95ee2d4f36bb9356b2b86537f9384e85b828d25
rodrig1509 <digosmantovanello@gmail.com>
2024-08-05 08:56:18 -0300

    mais javascript array + loop for

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-console.log("Bem vindo novamente!!!!");
+console.log("Bem vindo novamente!");
+
+//Array básico de frutas
+
+let frutas = ["banana", "laranja", "caqui", "morango", "uva"];
+console.log(frutas)
+console.table(frutas)
+console.log(frutas[0])
+
+//lendo array com loop FOR
+for (let x = 0; x < frutas.length; x++) {
+    console.log(frutas[x]);
+}
